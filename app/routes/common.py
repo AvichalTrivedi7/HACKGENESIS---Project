243e85ae033df68af6f2b90f4b3df14faceb39a3
AvichalTrivedi7 <avichaltrivedi7@gmail.com>
2025-09-23 02:03:44 +0530

    Additions

--- app/routes/common.py
+++
@@ -1,8 +0,0 @@
-# app/routes/common.py
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/common", tags=["Common APIs"])
-
-@router.get("/ping")
-def ping():
-    return {"message": "Backend is alive 🚀"}
